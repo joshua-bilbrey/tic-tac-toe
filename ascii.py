b95d82124f49f907f6a3ab16c932f51c54f6b733
Joshua Bilbrey <97485531+joshua-bilbrey@users.noreply.github.com>
2022-05-09 12:33:50 -0700

    Initial commit

---
+++ ascii.py
@@ -0,0 +1,39 @@
+logo = '             Tic Tac Toe\n\n\n'
+
+tile_empty = '''      
+      
+      
+      '''
+
+tile_x = '''x    x
+  xx  
+  xx  
+x    x'''
+
+tile_o = ''' oooo 
+o    o
+o    o
+ oooo '''
+
+tile_select_x = '''@@  @@
+  @@  
+  @@  
+@@  @@'''
+
+tile_select_o = ''' @@@@ 
+@@  @@
+@@  @@
+ @@@@ '''
+
+board_arm = '''__________|__________|__________'''
+
+board_leg = '''  |  
+  |  
+  |  
+  |  '''
+
+tile_dict = {0: tile_empty,
+             1: tile_x,
+             2: tile_o,
+             3: tile_select_x,
+             4: tile_select_o}
